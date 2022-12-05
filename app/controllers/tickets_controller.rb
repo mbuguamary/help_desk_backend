d23f62001ec03eac0d215108a0d243c3cf399a58
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-12-05 16:41:06 +0300

    created the crude operations

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,47 @@
 class TicketsController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unproccessable_entity_response 
+     def index
+        render json: Ticket.all, status: :ok
+     end
+    def create
+        ticket = Ticket.create!(ticket_params)
+         render json: ticket status: :created    
+     end
+     def show
+        ticket = Ticket.find_by(id: params(:id))
+        if ticket
+            render json: ticket
+        else
+            render json: { error: "Ticket not found" }, status: :not_found   
+        end
+     end
+     def destroy
+
+     end
+     def update
+        ticket = Ticket.find_by(id: params[:id])
+        if ticket
+            ticket.update!(ticket_params)
+            render json: ticket
+        else
+            render json: { error: "Ticket not found" }, status: :not_found
+        end
+      end
+      def destroy
+        ticket= Ticket.find_by(id: params[:id])
+        if ticket
+          ticket.destroy
+          head :no_content
+        else
+         render json: {error: "restaurant not found"}, status: :not_found
+        end
+      end
+  
+    private
+    def ticket_params
+        params.permit(:email, :name, :department_id, :phone_no, :summary, :description, category_id, priority, :notes, :status)
+    end
+  def render_unproccessable_entity_response(error)
+    render json: { errors: error.record.errors.full_messages }, status: :unprocessable_entity
+    end
 end
