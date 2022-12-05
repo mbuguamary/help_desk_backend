101fff5ee125520bbb642424b5539494261b2d81
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-12-05 16:40:33 +0300

    creted crude operations

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,17 @@
+class CategoriesController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unproccessable_entity_response    
+    def index
+        render json: Category.all, status: :ok
+    end
+    def create 
+        category = Category.create(cat_params)
+        render json : category, status: :ok
+    end
+    private
+ def cat_params
+    params.permit(:category_name)
+ end
+ def render_unproccessable_entity_response(error)
+    render json: { errors: error.record.errors.full_messages }, status: :unprocessable_entity
+    end
+end
