d9277c57e84b884b8533d98ffced948c7c39c903
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-12-05 16:44:38 +0300

    created tickets migration

--- db/migrate/20221205121102_create_tickets.rb
+++ db/migrate/20221205121102_create_tickets.rb
@@ -1,6 +1,15 @@
 class CreateTickets < ActiveRecord::Migration[7.0]
   def change
     create_table :tickets do |t|
+      t.string :name
+      t:string :department_id
+      t.string :phone_no
+      t.text :summary
+      t.text :description
+      t.string :category_id
+      t.string :priority
+      t.string :notes
+      t.string :status
 
       t.timestamps
     end
