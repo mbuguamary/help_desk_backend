cc42364fcad7c6d2baf6bbf85a9609701db56afc
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-12-05 16:40:48 +0300

    created crude operations

---
+++ app/controllers/departments_controller.rb
@@ -0,0 +1,19 @@
+class DepartmentsController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unproccessable_entity_response 
+    def index
+        render json: Department.all, status: :ok
+    end
+
+    def create
+        department = Department.create(dept_params)
+        render json:department, status: :created
+    end
+
+    private
+     def dept_params
+        params.permit(:department_name)
+     end
+    def render_unproccessable_entity_response(error)
+        render json: { errors: error.record.errors.full_messages }, status: :unprocessable_entity
+        end
+end
