5e6ca5cc2d52c7cc4bc0d44acd3666959a612fcf
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-12-08 10:07:35 +0300

    removed column

--- db/migrate/20221205121102_create_tickets.rb
+++ db/migrate/20221205121102_create_tickets.rb
@@ -1,8 +1,9 @@
 class CreateTickets < ActiveRecord::Migration[7.0]
   def change
     create_table :tickets do |t|
+      t.string :email
       t.string :name
-      t:string :department_id
+      t.string :department_id
       t.string :phone_no
       t.text :summary
       t.text :description
@@ -10,7 +11,6 @@ class CreateTickets < ActiveRecord::Migration[7.0]
       t.string :priority
       t.string :notes
       t.string :status
-
       t.timestamps
     end
   end
