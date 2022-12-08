9694f1be9b04c58a2781c28f96b8d9a6c912172a
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-12-08 10:09:38 +0300

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :departments
-  resources :categories
-  resources :tickets
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/tickets", to: "tickets#index"
+  get "/tickets/:id", to: "tickets#show"
+  post "/tickets", to: "tickets#create"
+  patch "/tickets/:id", to: "tickets#update"
+  delete "/tickets/:id", to: "tickets#destroy"
+  get "departments", to: "departments#index"
+  post "departments", to: "departments#create"
+  get "categories", to: "categories#index"
+  post "categories", to: "categories#create"
+  get "/me", to: "users#show"
+  post "/signup", to: "users#create"
+  post "/login", to: "sessions#create"
+
 end
