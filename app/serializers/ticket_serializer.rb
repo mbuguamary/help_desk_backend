fd9ac16a0a36708d305384cfe1ad8f139c42ce4e
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-12-09 15:10:36 +0300

    added columns

--- app/serializers/ticket_serializer.rb
+++ app/serializers/ticket_serializer.rb
@@ -1,3 +1,3 @@
 class TicketSerializer < ActiveModel::Serializer
-  attributes :id, :summary, :name, :priority, :status, :created_at, :updated_at
+  attributes :id, :name, :department_id, :phone_no, :summary, :description, :category_id, :priority, :status, :created_at, :updated_at
 end
